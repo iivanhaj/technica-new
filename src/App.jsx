42d665e0059994fc7a628d8c09d85307498efdf4
Jahnavi Majumder <majumderj811@gmail.com>
2023-08-05 17:40:56 +0530

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,29 +13,26 @@ import TrackCard from './Trackcard.jsx';
 const App = () => {
   return (
     <div className="relative min-h-screen bg-primary">
-      {/* Background Image */}
-      <div
-        className="absolute inset-0 z-0 bg-cover bg-center"
-        style={{ backgroundImage: `url(${bg_image})` }}
-      ></div>
+      {/* Background Image Container */}
+      <div className="absolute inset-0 z-0 bg-cover bg-center" style={{ backgroundImage: `url(${bg_image})` }}>
+        {/* Rover on Top Left */}
+        <div className="z-10 absolute top-0 left-0 w-32 sm:w-40 md:w-48 lg:w-56 xl:w-64">
+          <img src={rover_image} alt="Rover" className="w-full h-full object-cover object-center" />
+        </div>
 
-      {/* Rover on Top Left */}
-      <div className="z-10 absolute top-0 left-0 w-32 sm:w-40 md:w-48 lg:w-56 xl:w-64">
-        <img src={rover_image} alt="Rover" className="w-full h-full object-cover object-center" />
-      </div>
+        {/* Planet on Top Right */}
+        <div className="z-10 absolute top-0 right-0 w-16 sm:w-20 md:w-24 lg:w-32 xl:w-36">
+          <img src={planet_1} alt="Planet1" className="w-full h-full object-cover object-center" />
+        </div>
 
-      {/* Planet on Top Right */}
-      <div className="z-10 absolute top-0 right-0 w-16 sm:w-20 md:w-24 lg:w-32 xl:w-36">
-        <img src={planet_1} alt="Planet1" className="w-full h-full object-cover object-center" />
-      </div>
-
-      {/* Planet on Bottom Left */}
-      <div className="z-10 absolute bottom-0 left-0 w-16 sm:w-20 md:w-24 lg:w-32 xl:w-36">
-        <img src={planet_2} alt="Planet2" className="w-full h-full object-cover object-center" />
+        {/* Planet on Bottom Left */}
+        <div className="z-10 absolute bottom-0 left-0 w-16 sm:w-20 md:w-24 lg:w-32 xl:w-36">
+          <img src={planet_2} alt="Planet2" className="w-full h-full object-cover object-center" />
+        </div>
       </div>
 
       {/* Track Cards */}
-      <div className="z-10 absolute top-1/2 right-1/4 -mt-40">
+      <div className="z-10 absolute top-1/2 right-1/4 -mt-40 sm:-ml-12 md:-ml-14 lg:-ml-16 xl:-ml-18">
         <TrackCard title="TRACK 01" icon={scholar_image} positionRight={true} />
         <TrackCard title="TRACK 02" icon={education_image} positionRight={false} />
         <TrackCard title="TRACK 03" icon={medical_image} positionRight={true} />
--- src/Trackcard.jsx
+++ src/Trackcard.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react'; // Import useState
 
 const TrackCard = ({ title, icon, positionRight }) => {
-  const iconPosition = positionRight ? 'flex-row-reverse' : 'flex-row'; // Adjust icon position based on prop
+  const [isHovered, setIsHovered] = useState(false); // State to track hover
+
+  const iconPosition = positionRight ? 'flex-row-reverse' : 'flex-row';
+  const titleMarginClass = (title === 'TRACK 01' || title === 'TRACK 03') ? 'mr-4' : '';
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
 
   return (
-    <div className={`bg-white rounded-lg p-4 flex items-center ${iconPosition} mb-6 shadow-md`}>
-      <img src={icon} alt={title} className="w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20 mr-4" />
-      <span className="font-poppins text-lg sm:text-xl">{title}</span>
+    <div
+      className={`bg-transparent rounded-lg p-4 flex items-center ${iconPosition} mb-6 shadow-md`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <img
+        src={icon}
+        alt={title}
+        className={`w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20 mr-4 transition-transform ${
+          isHovered ? 'scale-110' : 'scale-100'
+        }`}
+      />
+      <span
+        className={`font-poppins text-white text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl ${titleMarginClass}`}
+      >
+        {title}
+      </span>
     </div>
   );
 };
