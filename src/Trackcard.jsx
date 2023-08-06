f61fb396b0ca1ef55659e826b3554e7350388897
K-Sikaria <128220424+K-Sikaria@users.noreply.github.com>
2023-08-06 05:21:14 +0000

    sixth commit

--- src/Trackcard.jsx
+++ src/Trackcard.jsx
@@ -4,7 +4,7 @@ const TrackCard = ({ title, icon, positionRight }) => {
   const [isHovered, setIsHovered] = useState(false); // State to track hover
 
   const iconPosition = positionRight ? 'flex-row-reverse' : 'flex-row';
-  const titleMarginClass = (title === 'TRACK 01' || title === 'TRACK 03') ? 'mr-4' : '';
+  const titleMarginClass = (title === 'TRACK 01' || title === 'TRACK 03') ? 'mr-2' : '';
 
   const handleMouseEnter = () => {
     setIsHovered(true);
@@ -16,18 +16,18 @@ const TrackCard = ({ title, icon, positionRight }) => {
 
   return (
     <div
-      className={`bg-transparent rounded-lg p-4 flex items-center ${iconPosition} w-64 sm:w-72 md:w-80 lg:w-96 xl:w-108 mb-4 md:mb-6 shadow-md`}
+      className={`bg-transparent rounded-lg p-4 flex items-center ${iconPosition} w-56 sm:w-64 md:w-72 lg:w-80 xl:w-96 mb-4 md:mb-6 shadow-md`}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
       <img
         src={icon}
         alt={title}
-        className={`w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20 mr-4 transition-transform ${
+        className={`w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14 mr-4 transition-transform ${
           isHovered ? 'scale-110' : 'scale-100'
         }`}
       />
-      <span className="flex items-center text-center"> {/* Flex container to center the title */}
+      <span className="flex items-center text-center">
         <span
           className={`font-poppins text-white text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl ${titleMarginClass}`}
         >
