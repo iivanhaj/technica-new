ce2c277c1efabe334dfaee9a64a739ad117cd6f5
Jahnavi Majumder <majumderj811@gmail.com>
2023-08-08 14:42:37 +0000

    changed commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import bg_image from './bg.png';
-import planet_1 from './planet_tr.png';
-import planet_2 from './planet_bl.png';
-import planets from './Small_planets.png';
-import rover_image from './rover.png';
+import bg_image from './frame1.png';
+import bg_image2 from './frame2.png';
 import scholar_image from './scholar.png';
 import medical_image from './medical.png';
 import education_image from './education.png';
@@ -12,46 +9,36 @@ import TrackCard from './Trackcard.jsx';
 
 const App = () => {
   return (
-    <div className="relative h-screen bg-primary">
-      {/* Background Image Container */}
-      <div
-        className="absolute inset-0 z-0 bg-cover bg-center h-full"
-        style={{ backgroundImage: `url(${bg_image})` }}
-      >
-        {/* Rover on Top Left */}
-        <div 
-          className="z-10 absolute top-0 left-0 w-full overflow-hidden">
-          <img src={rover_image} alt="Rover" className="w-full h-full object-cover object-center" />
-        </div>
-
-        {/* Planet on Top Right */}
+    <>
+      <div className="h-screen bg-primary relative">
+        {/* Background Image Container */}
         <div
-          className="z-10 absolute top-0 right-0 w-16 sm:w-20 md:w-24 lg:w-32 xl:w-36 overflow-hidden"
-          style={{ height: '40%', marginTop: '-10%', right: 0 }}
-        >
-          <img src={planet_1} alt="Planet1" className="w-full h-full object-cover object-center" />
-        </div>
+          className="absolute inset-0 z-0 bg-cover"
+          style={{
+            backgroundImage: `url(${bg_image})`,
+          }}
+        ></div>
 
-        {/* Planet on Bottom Left */}
-        <div className="z-10 absolute bottom-24 left-8 sm:left-10 md:left-12 lg:left-14 xl:left-16 w-16 sm:w-20 md:w-24 lg:w-32 xl:w-36">
-          <img src={planet_2} alt="Planet2" className="w-full h-full object-cover object-center" />
-        </div>
-
-        {/* Small Planets */}
-        <div className="z-10 absolute bottom-12 left-24 sm:left-26 md:left-28 lg:left-30 xl:left-32 w-18 sm:w-23 md:w-28 lg:w-32 xl:w-37">
-          <img src={planets} alt="Planets" className="w-full h-full object-cover object-center" />
+        {/* Track Cards */}
+        <div className="z-10 absolute top-40 right-40 mr-4 sm:mr-8 md:mr-12 lg:mr-16 xl:mr-20 flex flex-col space-y-4 items-end sm:items-center">
+          <TrackCard title="TRACK 01" icon={scholar_image} positionRight={true} />
+          <TrackCard title="TRACK 02" icon={education_image} positionRight={true} />
+          <TrackCard title="TRACK 03" icon={medical_image} positionRight={true} />
+          <TrackCard title="TRACK 04" icon={currency_image} positionRight={true} />
         </div>
       </div>
-
-      {/* Track Cards */}
-      <div className="z-10 absolute  top-1/2 -mt-72 sm:-mt-64 md:-mt-56 lg:-mt-48 xl:-mt-40 right-24 sm:right-25 md:right-26 lg:right-28 xl:right-29">
-        <TrackCard title="TRACK 01" icon={scholar_image} positionRight={true} />
-        <TrackCard title="TRACK 02" icon={education_image} positionRight={false} />
-        <TrackCard title="TRACK 03" icon={medical_image} positionRight={true} />
-        <TrackCard title="TRACK 04" icon={currency_image} positionRight={false} />
+      <div className="h-screen bg-primary relative">
+        {/* Background Image Container */}
+        <div
+          className="absolute inset-0 z-0 bg-cover"
+          style={{
+            backgroundImage: `url(${bg_image2})`,
+          }}
+        ></div>
       </div>
-    </div>
+    </>
   );
 };
 
 export default App;
+
